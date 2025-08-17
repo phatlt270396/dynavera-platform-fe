fdc9b73698c5922d064e6cd62a1821ece2044b99
Khoi <khoi5405@gmail.com>
2025-08-17 14:06:35 +0700

    updated taiwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['~/assets/css/tailwind.css'],
-
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
