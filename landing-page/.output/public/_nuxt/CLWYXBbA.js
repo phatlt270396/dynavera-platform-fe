08f28f4b5100c3165e8aec7857dccd5c001bcb0d
Hans Lee <hanslee@Hanss-MacBook-Pro.local>
2025-08-21 09:48:34 +0700

    Update landing page routing and build static files

--- landing-page/.output/public/_nuxt/CLWYXBbA.js
+++
@@ -1,1 +0,0 @@
-import{x as e,y as t}from"./DxRvC7wx.js";const o={__name:"index",setup(r){return(a,c)=>(t(),e("div"))}};export{o as default};
--- pages/home/index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div>
-    </div>
-</template>
-  
-<script setup>
-// Không cần logic JS cho trang tĩnh
-</script>
-  
